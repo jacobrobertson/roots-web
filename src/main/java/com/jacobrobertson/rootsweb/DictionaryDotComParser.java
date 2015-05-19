5b46df28259a93da7d53fd77eac0155f85afe03f
jacobrobertson <jacob.robertson.work@gmail.com>
2015-05-18 22:07:11 -0500

    dotcom

---
+++ src/main/java/com/jacobrobertson/rootsweb/DictionaryDotComParser.java
@@ -0,0 +1,39 @@
+package com.jacobrobertson.rootsweb;
+
+import java.util.regex.Pattern;
+
+public class DictionaryDotComParser {
+
+	private Pattern originPattern = Pattern.compile("<div class=\"tail-box tail-type-origin\">");
+	public void parsePage(String text) {
+		/*	<div class="tail-box tail-type-origin">
+		 	<div class="tail-header">
+		 	<span>
+		 		<span class="oneClick-link oneClick-available">Word</span> 
+		 		<span class="oneClick-link oneClick-available">Origin</span> 
+		 	</span>
+		 	</div>
+			<div class="tail-content">
+				<div class="tail-elements">
+					<span>
+					<span class="oneClick-link">C19:</span> 
+					<span class="oneClick-link oneClick-available">New</span> 
+					<span class="oneClick-link oneClick-available">Latin,</span> 
+					<span class="oneClick-link">from</span> 
+					</span>
+					<span class="dbox-sc">
+					<span>
+					<span class="oneClick-link oneClick-available">epi-</span> 
+					</span>
+					</span>
+					<span>+ <span class="oneClick-link oneClick-available">Greek</span> </span>
+					<span class="dbox-italic"><span><span class="oneClick-link oneClick-available">kardia</span> </span></span>
+					<span><span class="oneClick-link oneClick-available">heart</span> </span>
+				</div>
+			</div>
+			</div>
+		*/
+		
+	}
+	
+}
